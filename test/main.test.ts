c2f25a78fcd3d97d85fe9be92dbaf722e84e4e17
Jaidlab <240663663+Jaidlab@users.noreply.github.com>
2026-04-12 13:55:52 +0100

    Removed zeug dependency from test code
    Signed-off-by: Jaidlab <240663663+Jaidlab@users.noreply.github.com>

--- test/main.test.ts
+++ test/main.test.ts
@@ -2,8 +2,7 @@ import {expect, it} from 'bun:test'
 
 import {getMainModuleDefault} from 'zeug'
 
-const test = await getMainModuleDefault<typeof import('test')>('test')
-
+const {default: test} = await import('#src/main.ts')
 it('should run', () => {
   expect(test).toBe(1) // TODO Test actual functionality
 })
