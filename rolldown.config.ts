190f749464103715b942b8c0a2cdecf99a88d577
Jaidlab <240663663+Jaidlab@users.noreply.github.com>
2026-05-23 01:24:58 +0200

    Triggered a commit for testing purposes

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -1,7 +1,9 @@
 import type {RolldownOptions} from 'rolldown'
 
+import bundilPlugin from 'rolldown-plugin-bundil'
+
 const config: RolldownOptions = {
-  plugins: []
+  plugins: [bundilPlugin()],
 }
 
 export default config
